3f6d1bddd3fa44125342ba68d75accb9021eac76
Francisco Albuquerque <92957206+Sblck@users.noreply.github.com>
2025-07-04 13:54:52 +0100

    Update testes_main_alunos.py
    Generalização de testes

--- testes_main_alunos.py
+++ testes_main_alunos.py
@@ -5,49 +5,105 @@ from main import calculadora, calculadora_v2, calculadora_v3, calculadora_v4
 
 class TestCalculadora(unittest.TestCase):
 
+
     def teste_operacoes_basicas(self):
-        # Teste operações básicas de cada operador + - * / % ^
-        self.assertEqual(calculadora(2, 3, '+'), 5)
+        valores = [2, -2, 2.5, -2.5]
+        for a in valores:
+            for b in valores:
+                # Teste operações básicas de cada operador + - * / % ^
+                self.assertEqual(calculadora(a, b, '+'), a+b)
+                self.assertEqual(calculadora(a, b, '-'), a-b)
+                self.assertEqual(calculadora(a, b, '*'), a*b)
+                self.assertEqual(calculadora(a, b, '/'), a/b)
+                self.assertEqual(calculadora(a, b, '%'), a%b)
+                self.assertEqual(calculadora(a, b, '^'), a^b)
 
     def teste_v2_operacoes(self):
-        # Teste operações básicas de cada operador + - * / % ^
-        self.assertEqual(calculadora_v2(2, 3, '+'), 5)
+        valores = [2, -2, 2.5, -2.5]
+        for a in valores:
+            for b in valores:
+                # Teste operações básicas de cada operador + - * / % ^
+                self.assertEqual(calculadora_v2(a, b, '+'), a+b)
+                self.assertEqual(calculadora_v2(a, b, '-'), a-b)
+                self.assertEqual(calculadora_v2(a, b, '*'), a*b)
+                self.assertEqual(calculadora_v2(a, b, '/'), a/b)
+                self.assertEqual(calculadora_v2(a, b, '%'), a%b)
+                self.assertEqual(calculadora_v2(a, b, '^'), a^b)
 
     def teste_v3_operacoes(self):
-        # Teste operações básicas de cada operador + - * / % ^
-        self.assertEqual(calculadora_v3(2, 3, '+'), 5)
+        valores = [2, -2, 2.5, -2.5]
+        for a in valores:
+            for b in valores:
+                # Teste operações básicas de cada operador + - * / % ^
+                self.assertEqual(calculadora_v3(a, b, '+'), a+b)
+                self.assertEqual(calculadora_v3(a, b, '-'), a-b)
+                self.assertEqual(calculadora_v3(a, b, '*'), a*b)
+                self.assertEqual(calculadora_v3(a, b, '/'), a/b)
+                self.assertEqual(calculadora_v3(a, b, '%'), a%b)
+                self.assertEqual(calculadora_v3(a, b, '^'), a^b)
 
     def teste_v4_operacoes(self):
-        # Teste operações básicas de cada operador + - * / % ^
-        self.assertEqual(calculadora_v4(2, 3, '+'), 5)
+        valores = [2, -2, 2.5, -2.5]
+        for a in valores:
+            for b in valores:
+                # Teste operações básicas de cada operador + - * / % ^
+                self.assertEqual(calculadora_v4(a, b, '+'), a+b)
+                self.assertEqual(calculadora_v4(a, b, '-'), a-b)
+                self.assertEqual(calculadora_v4(a, b, '*'), a*b)
+                self.assertEqual(calculadora_v4(a, b, '/'), a/b)
+                self.assertEqual(calculadora_v4(a, b, '%'), a%b)
+                self.assertEqual(calculadora_v4(a, b, '^'), a^b)
 
     def teste_operacoes_diversas(self):
-        # Teste divisão por zero operador para todas versões / %
-        self.assertTrue(math.isnan(calculadora(5, 0, '/')))
-        self.assertTrue(math.isnan(calculadora(5, 0, '%')))
+        valores = [0, 5, -5, 5.2, -5.2]
+        for funcao in [calculadora, calculadora_v2, calculadora_v3, calculadora_v4]:
+            for a in valores:
+                # Teste divisão por zero operador para todas versões / %
+                self.assertTrue(math.isnan(calculadora(a, 0, '/')))
+                self.assertTrue(math.isnan(calculadora(a, 0, '%')))
 
         # Teste operador inválido - fazer três testes para todas as versões
-        self.assertTrue(math.isnan(calculadora(2, 3, '$')))
-        self.assertTrue(math.isnan(calculadora(2, 5, '#')))
-        self.assertTrue(math.isnan(calculadora(0, 2, 'qwe')))
+        valores = [0,2, -2, 2.5, -2.5]
+        for funcao in [calculadora_v2, calculadora_v3, calculadora_v4]:
+            for a in valores:
+                for b in valores:
+                    self.assertTrue(math.isnan(funcao(a, b, '$')))
+                    self.assertTrue(math.isnan(funcao(a, b, '#')))
+                    self.assertTrue(math.isnan(funcao(a, b, 'qwe')))
 
         # Teste números de virgula flutuante - fazer três testes para todas as versões
-        self.assertAlmostEqual(calculadora(2.5, 1.5, '+'), 4.0)
-        self.assertAlmostEqual(calculadora(4.5, 1.5, '-'), 3.0)
-        self.assertAlmostEqual(calculadora(5.5, 1.5, '*'), 8.25)
+
+        '''
+        
+        Nota : Testes anterior já deveram apanhar estes casos;
+
+        #self.assertAlmostEqual(calculadora(2.5, 1.5, '+'), 4.0)
+        #self.assertAlmostEqual(calculadora(4.5, 1.5, '-'), 3.0)
+        #self.assertAlmostEqual(calculadora(5.5, 1.5, '*'), 8.25)
 
         # Teste números negativos - fazer 3 testes para todas as versões
-        self.assertEqual(calculadora(-2, 3, '*'), -6)
+        #self.assertEqual(calculadora(-2, 3, '*'), -6)
 
         # Teste números negativos com divisão e módulo, testar para todas as versões
-        self.assertTrue(calculadora(-6, 3, '/'), -2.0)
-        self.assertTrue(calculadora(-7, 3, '%'), 2.0)
+        #self.assertTrue(calculadora(-6, 3, '/'), -2.0)
+        #self.assertTrue(calculadora(-7, 3, '%'), 2.0)
 
         # Teste números negativos com exponenciação, testar para todas as versões
-        self.assertEqual(calculadora(-2, 3, '^'), -8)
+        #self.assertEqual(calculadora(-2, 3, '^'), -8)
 
+        '''
         # Teste números negativos com exponenciação de zero, testar para todas as versões
-        self.assertEqual(calculadora(0, 3, '^'), 0)
+        valores = [0, -3, -4.2]
+        for funcao in [calculadora, calculadora_v2, calculadora_v3, calculadora_v4]:
+            for a in valores:
+                #a^0 = 1
+                self.assertEqual(funcao(a, 0, '^'), 1)
+            for b in valores:
+                # 0^b = 0 para b > 0, 0^0 == 1
+                if b == 0:
+                    self.assertEqual(funcao(0, b, '^'), 1)
+                else:
+                    self.assertEqual(funcao(0, b, '^'), 0)
 
 
 if __name__ == '__main__':
